1fb2570236322ec7c20cdaae44cdba88ef513acb
czx <927118139@qq.com>
2020-05-17 20:19:25 +0800

    's'

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
-    
+    <ul>
+      <li>首页</li>
+      <li>首页</li>
+      <li>首页</li>
+     <li>首页</li>
+    </ul>
   </div>
 </template>
 
 <style>
-
+@import "./assets/css/app.css";
 
 </style>

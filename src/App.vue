92f198908d9ab30bb8b50f844ca4ea15cfb71373
czx <927118139@qq.com>
2020-05-18 10:06:00 +0800

    '添加'

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    <ul>
-      <li>首页</li>
-      <li>首页</li>
-      <li>首页</li>
-     <li>首页</li>
-    </ul>
+    <tab></tab>
   </div>
 </template>
 
+<script>
+import tab from './components/tab/tab.vue'
+export default {
+  name:'app',
+  components:{
+    tab
+  }
+}
+</script>
 <style>
 @import "./assets/css/app.css";
 

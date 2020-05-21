8794d13c9d9216e068a881bb8592dfe879250283
czx <927118139@qq.com>
2020-05-21 15:22:52 +0800

    '添加'

---
+++ src/views/homes/Home.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="home">
+        <navtab class="nav_tab"><div slot="content">购物街</div></navtab>
+        <!-- <homeswiper :banners='banners' class="swiper"></homeswiper> -->
+        <swiper>
+            <swiper-item v-for='item in banners' :key="item.title">
+                <a :href="item.link">
+                    <img :src="item.image" alt="">
+                </a>
+            </swiper-item>
+        </swiper>
+        <homerecommend :recommends="recommends"></homerecommend>
+    </div> 
+</template>
+<script>
+import navtab from 'components/common/navtab/navtab'
+import { Swiper, SwiperItem } from 'components/common/swiper'
+// import homeswiper from './childrenhome/homeswiper'
+import homerecommend from './childrenhome/homerecommend'
+import {GetHomeMutliData} from 'network/home.js'
+export default {
+    name:'Home',
+    components:{
+        navtab,
+        // homeswiper,
+        Swiper,
+        SwiperItem,
+        homerecommend
+    },
+    data(){
+        return{
+            //存储轮播数据
+            banners :[],
+            //存储轮播下面的小图片
+            recommends: []
+        }
+    },
+    created(){
+        //函数调用 -> 压入函数栈（保存调用函数中所有变量）
+        //函数调用结束 -> 弹出栈（释放所有变量）
+        //当函数调用完之后，里面的变量都会被回收，所有要使用变量存储起来
+        GetHomeMutliData()
+        .then( res =>{
+            // console.log(res.data.banner.list)
+            //当函数调用完之后，里面的变量都会被回收，所有要使用变量存储起来
+            //轮播数据
+            this.banners = res.data.banner.list
+            this.recommends = res.data.recommend.list
+            
+        })
+        .catch( err =>{
+            console.log(err)
+        })
+    }
+}
+</script>
+<style scoped>
+ .nav_tab{
+    background-color: var(--color-tint);
+    color: white;
+ }
+ .swiper{
+     margin-top: 2px;
+ }
+</style>
